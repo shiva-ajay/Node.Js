c12c0739de6ba0d6b17d30d59bd04b4bda291243
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-25 20:31:12 +0530

    HTTP Module & Create Server

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 404;
+  // res.setHeader('Content-Type', 'text/html');
+  // res.write('Hello World');
+  // res.end('<h1>Hello World!</h1>');
+
+  res.writeHead(500, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Server Error' }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
