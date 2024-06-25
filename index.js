f58db0072ec85584e5fa6a50d4ec3ad40976a17a
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-25 20:03:08 +0530

    ES Modules

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
-console.log(`Random Number: ${generateRandomNumber()}`);
-console.log(`Celcius to fahrenhiet: ${celciusToFahrenheit (0)}`);
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts());
+console.log(`Posts Length: ${getPostsLength()}`);
+
+
+
+// const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celcius to fahrenhiet: ${celciusToFahrenheit (0)}`);
---
+++ postController.js
@@ -0,0 +1,12 @@
+const posts = [
+    { id: 1, title: 'Post One' },
+    { id: 2, title: 'Post Two' },
+];
+
+const getPosts = () => posts;
+
+export const getPostsLength = () => posts.length;
+
+export default getPosts;
+
+// export const getPosts = () => posts;
