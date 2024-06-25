91668becc8c73dbbd263b5c1db7fc99a0b51544c
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-25 20:53:47 +0530

    node

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const server = http.createServer((req, res) => {
   // res.write('Hello World');
   // res.end('<h1>Hello World!</h1>');
 
-  res.writeHead(500, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Server Error' }));
+  // res.writeHead(500, { 'Content-Type': 'application/json' });
+  // res.end(JSON.stringify({ message: 'Server Error' }));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello World!</h1>");
 });
 
 server.listen(PORT, () => {
